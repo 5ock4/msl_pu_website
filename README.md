2bb089b72221a03933f8fbcf81adfe8cbb2851ae
adam.strakos <adam.strakos@notino.com>
2025-01-04 21:21:01 +0100

    feat: init wagtail app

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# msl-pu
-Website for firesport league: Moravskoslezská liga v PÚ
+# MSL PÚ website
+Website for firesport league: [Moravskoslezská liga v PÚ](https://www.youtube.com/watch?v=CtEhhjKSOGc&t=3s)
+
+
+## Menu Structure (Wagtail Apps)
+
+- **Aktuality**
+- **O MSL**
+  - Historie
+  - Bodové a fin. ohodnocení
+  - Účastníci
+  - Ligová kola
+- **Přihláška**
+- **Dokumenty**
+- **Výsledky**
+- **Tipování**
+  - Tipování
+  - Výsledky
---
+++ src/msl_pu_website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=2=w-8a-k#fte^(-xsl-cc)a%!&$^a)$_qb#s0oaz^c=qifbx6"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
