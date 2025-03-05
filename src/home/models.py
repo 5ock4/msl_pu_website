c8c4911e0746c1d046989db8b242fef0e2f86c04
5ock4 <strakosadam@gmail.com>
2025-03-05 13:46:46 +0100

    feat: mvp part 2 (#6)

--- src/home/models.py
+++ src/home/models.py
@@ -1,7 +1,11 @@
-from django.db import models
-
 from wagtail.models import Page
+
+from about_msl.models import Team
 
 
 class HomePage(Page):
-    pass
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["teams"] = Team.objects.all()
+        return context
+
